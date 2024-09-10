41f26c43ded5edbd961684d9b06981f673dc46e3
midori <104818618@student.swin.edu.au>
2024-09-10 23:51:50 +1000

    fix local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"]});
